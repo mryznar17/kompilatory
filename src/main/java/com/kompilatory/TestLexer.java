8326f49032bf62af2911c60f0f739b88c6246fae
Marek <mryznar17@gmail.com>
2015-06-11 22:59:48 +0200

    Praca nad mainem

--- src/main/java/com/kompilatory/TestLexer.java
+++ src/main/java/com/kompilatory/TestLexer.java
@@ -1,9 +1,26 @@
 package com.kompilatory;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.BufferUnderflowException;
+
+import com.kompilatory.lexers.SqlLexer;
+
 public class TestLexer {
 
-	public static void main(String[] args) {
-		System.out.println("Chwalmy smak chuja");
+	public static void main(String[] args) throws IOException {
+		String init="init.sql";
+		BufferedReader buffer = new BufferedReader(new FileReader(init));
+		System.out.println(buffer.readLine());
+		
+		int c=0;
+		while((c = buffer.read()) != -1) 
+		{
+			char buf = (char) c;
+			SqlLexer scanner = new SqlLexer();
+		}
 
 	}
 
